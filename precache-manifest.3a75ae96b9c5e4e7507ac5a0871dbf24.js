9915974bad6b397c95e46abefd4fa3536fe3cceb
Salohy Privat <salohyprivat@gmail.com>
2019-01-10 23:32:57 +0100

    Updates

---
+++ precache-manifest.3a75ae96b9c5e4e7507ac5a0871dbf24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d3bae8fa3bcec3b10c4",
+    "url": "/velirano/static/js/runtime~main.5d3bae8f.js"
+  },
+  {
+    "revision": "3254906bde58f4f63806",
+    "url": "/velirano/static/js/main.3254906b.chunk.js"
+  },
+  {
+    "revision": "df9630f50098b1e592b4",
+    "url": "/velirano/static/js/1.df9630f5.chunk.js"
+  },
+  {
+    "revision": "3254906bde58f4f63806",
+    "url": "/velirano/static/css/main.853a12bc.chunk.css"
+  },
+  {
+    "revision": "df9630f50098b1e592b4",
+    "url": "/velirano/static/css/1.b33ed79e.chunk.css"
+  },
+  {
+    "revision": "04d3f1774752d1eb1ff84a423eaf9203",
+    "url": "/velirano/index.html"
+  }
+];
