81547e636cf9b9bb00dfe5f8dc9aef6b35079823
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2019-01-14 21:21:28 +0300

    Move src/components to src/Components

--- src/components/Enhancers/NavbarEnhancer.js
+++
@@ -1,4 +0,0 @@
-import { withProps } from "recompose";
-import { title } from "../../Constants/Velirano";
-
-export const withTitle = withProps(props => ({ title }));
--- src/components/Enhancers/VeliranoListEnhancer.js
+++
@@ -1,5 +0,0 @@
-import { withProps } from 'recompose';
-import { velirano, title } from '../../Constants/Velirano';
-
-export const withList = withProps(props => ({ list: velirano }));
-export const withTitle = withProps(props => ({ title }));
