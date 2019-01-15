bf9788c62d12cf8d46a8e6286b47f709f0533dfe
Tolotra Raharison <tolotra.raharison@shaft-it.com>
2019-01-15 13:12:48 +0100

    Add progressbar

--- src/Components/VeliranoItem.js
+++ src/Components/VeliranoItem.js
@@ -1,11 +1,37 @@
 import React from "react";
-import { Checkbox } from 'rimble-ui';
+import { Checkbox } from "rimble-ui";
+import { Circle } from "rc-progress";
 
-const VeliranoItem = props => (
-  <div style={{ minHeight: 100, display: 'flex', alignItems: 'center', borderBottom: 'solid 1px #E8E8E8' }}>
-    <Checkbox checked={props.item.done} onChange={() => {}} />
-    <div>
-      <div>{props.item.promise}</div>
+const VeliranoItem = ({ item }) => (
+  <div
+    style={{
+      minHeight: 100,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      borderBottom: "solid 1px #E8E8E8"
+    }}
+  >
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start"
+      }}
+    >
+      <Checkbox checked={item.percent === 100} onChange={() => {}} />
+      <div>
+        <div>{item.promise}</div>
+      </div>
+    </div>
+    <div style={{ width: 44, height: 44, minWidth: 44 }}>
+      <Circle
+        percent={item.percent}
+        strokeWidth="14"
+        strokeColor="#F8670D"
+        trailWidth="14"
+        trailColor="#EEEEEE"
+      />
     </div>
   </div>
 );
