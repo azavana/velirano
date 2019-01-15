c798c32e5ef528e663b40b050237efd8a3ef4ac2
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2019-01-15 22:57:34 +0300

    Feature: Add meter summary

---
+++ src/Components/Enhancers/MeterSummaryEnhancer.js
@@ -0,0 +1,13 @@
+import { withProps } from 'recompose';
+import { velirano } from '../../Constants/Velirano';
+
+const notStarted = velirano.filter(item => item.status === 'NOT_STARTED');
+const inProgress = velirano.filter(item => item.status === 'IN_PROGRESS');
+const achieved = velirano.filter(item => item.status === 'ACHIEVED');
+const broken = velirano.filter(item => item.status === 'BROKEN');
+
+export const withVelirano = withProps(props => ({ velirano }));
+export const withNotStarted = withProps(props => ({ notStarted }));
+export const withInProgress = withProps(props => ({ inProgress }));
+export const withAchieved = withProps(props => ({ achieved }));
+export const withBroken = withProps(props => ({ broken }));
--- src/Constants/Velirano.js
+++ src/Constants/Velirano.js
@@ -1,6 +1,8 @@
-export const title = "To-Do List ho an'i Prezidà Andry Rajoelina";
+export const title = "ToDo List ho an'i Prezidà Andry Rajoelina";
 
 export const sources = ["Madagascar Liberté, 1/10/19"];
+
+// status can be : NOT_STARTED, IN_PROGRESS, ACHIEVED, BROKEN
 
 export const velirano = [
   {
@@ -8,175 +10,204 @@ export const velirano = [
       "Buildings mora sy aparitaka eran'ny nosy : 50.000 logements ao anatin'ny 5 taona izany hoe eo amin'ny 27 logements par jour eo tokony hitsangana ",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "4*4 mandeha anaty rano sy miakatra amin'ny tendrombohitra",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Drône manenjika dahalo",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Miami Beach ao Toamasina",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Toilettes sy douches manara-penitra any Toamasina",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Palais des sports tahaka ny any France",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Côte d'Azur",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Parking eny amin'ireo EPP",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "SMIG 200.000 Ar",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise:
       "Projet Tanamasoandro : Tanàna lehibe ka hafindra eny avokoa ny birao-panjakana miampy autoroute",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Piscine olympique",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Puce atao amin’ny omby",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Tramway",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Fambolena coton de Tuléar",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Paris (Ambatondrazaka)",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Fizarana tany 1 ha isaky ny olona",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Hélicoptères 10",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Fitaovana manara-penitra ho an'ireo miaramila",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Fampiakarana ny vidin'ny la vanille",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Panneaux solaires hiadiana amin'ny delestazy",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Centrale Eolienne any Diégo",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Projet éthanol miaraka amin’i Mialy",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Lalana (Ambilobe-Vohémar, tetezana Ampasika, Anosizato)",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Kits scolaires + feutres",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Tsena mora misy vary mora, menaka mora",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Hanome asa sy fitaovana ho an'ireo tanora marobe",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Karaman'i Andry homena ny faritany 6",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Omena bourses ny mpianatra Ankatso",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   },
   {
     promise: "Foanana ny Sénat dia hanamboarana Oniversite ny tetibolan'ny Sénat",
     done: false,
     comment: "",
-    percent: 0
+    percent: 0,
+    status: "NOT_STARTED"
   }
 ];
